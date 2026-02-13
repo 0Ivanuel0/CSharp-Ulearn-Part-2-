142bc3c1a1a8ec982ce0b3f2725bb23800227ad3
Ivanuel <ladyginivan0@gmail.com>
2026-02-13 22:51:51 +0500

    1_and_2_practics

---
+++ Практики/1 Очереди Стеки Дженерики/Limited Size Stack/LimitedSizeStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+	LinkedList<T> stack;
+	int limit;
+
+	public LimitedSizeStack(int undoLimit)
+	{
+		if (limit < 0) throw new ArgumentException("limit cant be negative");
+
+		limit = undoLimit;
+		stack = new LinkedList<T>();
+	}
+
+	public void Push(T item)
+	{
+		if (limit == 0) return;
+
+		if (stack.Count == limit)
+			stack.RemoveLast();
+
+		stack.AddFirst(item);
+	}
+
+	public T Pop()
+	{
+		if (stack.Count == 0)
+			throw new InvalidOperationException("stack is empty");
+
+		var popElement = stack.First.Value;
+		stack.RemoveFirst();
+
+		return popElement;
+	}
+
+	public int Count => stack.Count;
+}
---
+++ Практики/1 Очереди Стеки Дженерики/Limited Size Stack/ListModel.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace LimitedSizeStack;
+
+public class ListModel<TItem>
+{
+	public List<TItem> Items { get; }
+	public int UndoLimit;
+
+	LimitedSizeStack<Command> UndoStack;
+
+	private struct Command
+	{
+		public readonly string Type;
+		public readonly TItem Item;
+		public readonly int Index;
+
+		public Command(string type, TItem item, int index)
+		{
+            Type = type;
+            Item = item;
+            Index = index;
+        }
+	}
+
+    public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
+	{
+	}
+
+	public ListModel(List<TItem> items, int undoLimit)
+	{
+		Items = items;
+		UndoLimit = undoLimit;
+		UndoStack = new LimitedSizeStack<Command>(undoLimit);
+    }
+
+	public void AddItem(TItem item)
+	{
+		UndoStack.Push(new Command("ADD", item, Items.Count - 1));
+        Items.Add(item);
+	}
+	
+	public void RemoveItem(int index)
+	{
+        UndoStack.Push(new Command("REMOVE", Items[index], index));
+        Items.RemoveAt(index);
+	}
+
+	public bool CanUndo()
+	{
+		return UndoStack.Count > 0;
+	}
+
+	public void Undo()
+	{
+		if (CanUndo() is false) 
+			throw new InvalidOperationException("Cancel cant be done");
+
+		var currentCommand = UndoStack.Pop();
+
+		if (currentCommand.Type == "ADD")
+			Items.RemoveAt(Items.Count - 1);
+		else Items.Insert(currentCommand.Index, currentCommand.Item);
+	}
+}
